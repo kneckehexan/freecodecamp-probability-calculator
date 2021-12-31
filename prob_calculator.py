699e600b556b4035b3d4c32c420bde59d78ee410
Philip Tunbjer <philip.tunbjer@gmail.com>
2022-01-01 00:46:34 +0100

    all tests pass

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,50 @@
 import copy
 import random
+from collections import Counter
 # Consider using the modules imported above.
 
 class Hat:
+    """Create a hat with different coloured balls of various amounts."""
+
+
+    def __init__(self, **kwargs):
+        contents = []
+        for key, val in kwargs.items():
+            for i in range(val):
+                contents.append(key)
+        self.contents = contents
+        
+    def draw(self, num):
+        """Draws a user specified number of balls from the hat at random."""
+
+
+        contents = self.contents
+        if num > len(contents):
+            return contents
+        
+        drawn = []
+        for i in range(num):
+            ballIndex = random.randrange(len(contents))
+            drawn.append(contents[ballIndex])
+            contents.pop(ballIndex)
+        
+        self.contents = contents
+        return drawn
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    """Execute a number of specified experiments on a pre determined hat and calculate the probability of getting the user specified balls."""
+
+
+    m = 0
+    for i in range(num_experiments):
+        hatCopy = copy.deepcopy(hat)
+        expCopy = copy.deepcopy(expected_balls)
+        drawn_balls= hatCopy.draw(num_balls_drawn)
+        for ball in drawn_balls:
+            if ball in expCopy:
+                expCopy[ball] -= 1
+        
+        if all(x <= 0 for x in expCopy.values()):
+            m += 1
+    return m/num_experiments
